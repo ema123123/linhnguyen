fd46f769c6b44eeeadb7fbc44b023039f22328b0
ema123123 <pqa.pht123@gmail.com>
2026-09-06 00:43:16 +0000

    ok

--- linhnguyen1/vite.config.ts
+++ linhnguyen1/vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        profile: fileURLToPath(new URL('./Linhnguyen858.html', import.meta.url)),
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
